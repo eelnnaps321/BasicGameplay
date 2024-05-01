ea044d06ca92efa45e83c01fc71953067b06a25a
leespann <spannl@roverkids.org>
2024-05-01 14:06:44 -0400

    2.4 done

--- BasicGameplay/Assets/scripts/DestroyOutOfBounds.cs
+++ BasicGameplay/Assets/scripts/DestroyOutOfBounds.cs
@@ -9,18 +9,20 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > 30)
+        //If an object goes past player, game ends
+        if (transform.position.z > topBound)
         {
             Destroy(gameObject);
         }
         else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over");
             Destroy(gameObject);
         }
     }
